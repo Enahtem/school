b1c23d080dcf149c278abe24dd7c3c211f1d39d2
Enahtem <enahtem.ch4@gmail.com>
2024-01-19 15:17:16 -0500

    school?

---
+++ Y1S2/ESC190/c_basics.c
@@ -0,0 +1,12 @@
+// Including Header Files
+#include <stdio.h> //I/O functions
+#include <stdlib.h> //Utility functions (Memory Allocation, Type Conversions, Algorithms)
+#include <math.h> //Math functions
+
+int main() {
+   printf("This is the \'Format String\'.\n");//Print Function
+   int a = 3;
+   char b = 'a';
+   printf("Printing char: %d. Printing int: %c", a, b);//Print Variables
+   return 0;
+}
--- Y1S2/ESC190/lab02.c
+++ Y1S2/ESC190/lab02.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-// ???????
-void print(char **address, int *string_size){
-    printf("Enter string length: ");
-    scanf("%d", string_size);
-    *address = (char *)malloc(*string_size * (sizeof(char)+1));
 
-    printf("Enter string char by char: ");
-    for (int i =0;i<*string_size;i++){
-        scanf("%s", &(*address)[i]);
-    }
-}
+
 
 int main(){
-    char *string;
-    int a;
-    print(&string, &a);
-    printf("STUFF HERE: %s", string);
-    free(string);
+    printf("STUFF HERE");
     return 0;
 }
